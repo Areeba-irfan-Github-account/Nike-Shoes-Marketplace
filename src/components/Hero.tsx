9064cf09f1a962d537c1545a585e4334ba818cc5
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2025-01-22 03:05:51 +0500

    Add files via upload

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import Category from './Category';
+import Hello from './Hello';
+import Main from './Main';
+
+const Hero = () => {
+    return (
+        <>
+            <Hello />
+            <Main />
+            <Category />
+        </>
+    );
+};
+
+export default Hero;
